5400a5f9ac0153d2bc3aaa330eca78c2a7e7da4e
あきわ <akiwa.inc.jp@gmail.com>
2018-08-22 14:14:19 +0900

    index.html から移行

--- wp/wp-content/themes/nagoya-partition/footer.php
+++ wp/wp-content/themes/nagoya-partition/footer.php
@@ -14,33 +14,16 @@
 
 ?>
 
-		</div><!-- #content -->
-
-		<footer id="colophon" class="site-footer" role="contentinfo">
-			<div class="wrap">
-				<?php
-				get_template_part( 'template-parts/footer/footer', 'widgets' );
-
-				if ( has_nav_menu( 'social' ) ) : ?>
-					<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
-						<?php
-							wp_nav_menu( array(
-								'theme_location' => 'social',
-								'menu_class'     => 'social-links-menu',
-								'depth'          => 1,
-								'link_before'    => '<span class="screen-reader-text">',
-								'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
-							) );
-						?>
-					</nav><!-- .social-navigation -->
-				<?php endif;
-
-				get_template_part( 'template-parts/footer/site', 'info' );
-				?>
-			</div><!-- .wrap -->
-		</footer><!-- #colophon -->
-	</div><!-- .site-content-contain -->
-</div><!-- #page -->
+<footer>
+	<div class="container">
+		<a class="logo" href="./"><img src="images/logo02.jpg" width="372" height="74" alt="名古屋パーテーション"></a>
+		<a href="https://akiwa.biz" target="_blank">あきわ株式会社</a>
+		<p>〒455-0831　愛知県名古屋市港区十一屋二丁目411-1<br>
+		TEL.052-381-4206　FAX.052-381-4207</p>
+		
+		<p id="copy">copyright(C)Nagoya Partition co., ltd. all rights reserved.</p>
+	</div>
+</footer>
 <?php wp_footer(); ?>
 
 </body>
