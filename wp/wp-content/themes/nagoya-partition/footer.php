24218e6f0e063044338f3a76ca7b3bfdb8e6bd58
あきわ <akiwa.inc.jp@gmail.com>
2018-08-22 14:39:22 +0900

    content_url() 追加

--- wp/wp-content/themes/nagoya-partition/footer.php
+++ wp/wp-content/themes/nagoya-partition/footer.php
@@ -16,7 +16,7 @@
 
 <footer>
 	<div class="container">
-		<a class="logo" href="./"><img src="images/logo02.jpg" width="372" height="74" alt="名古屋パーテーション"></a>
+		<a class="logo" href="./"><img src="<?php echo content_url() ?>/images/logo02.jpg" width="372" height="74" alt="名古屋パーテーション"></a>
 		<a href="https://akiwa.biz" target="_blank">あきわ株式会社</a>
 		<p>〒455-0831　愛知県名古屋市港区十一屋二丁目411-1<br>
 		TEL.052-381-4206　FAX.052-381-4207</p>
