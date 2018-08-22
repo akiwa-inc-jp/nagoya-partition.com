7d2b4bf6e7a404b5995db028c34b368d96a36507
あきわ <akiwa.inc.jp@gmail.com>
2018-08-22 14:11:42 +0900

    index.html 内容移行

--- wp/wp-content/themes/nagoya-partition/header.php
+++ wp/wp-content/themes/nagoya-partition/header.php
@@ -7,7 +7,7 @@
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package WordPress
- * @subpackage Twenty_Seventeen
+ * @subpackage nagoya-partition
  * @since 1.0
  * @version 1.0
  */
@@ -16,42 +16,50 @@
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyseventeen' ); ?></a>
+<body>
+	<header>
+		<div class="container clearfix">
+				<h1>名古屋パーテーション｜愛知・岐阜・三重・静岡エリアのオフィス、工場、ショップのパーテーション（パーティション）・間仕切りの設計施工・設置</h1>
+				<h2><a href="#"><img src="images/logo.png" width="268" height="48" alt="名古屋パーテーション"></a></h2>
+				<div class="h-right">
+					<a class="tel" href="tel:0120114912"><img src="images/tel.png" width="177" height="18" alt="TEL:0120-114-912"></a>
+					<a class="mail" href="contact.html"><img src="images/mail.png" width="130" height="29" alt="お問合せ"></a>
+					<span id="btn_menu"><i></i><span></span></span>
+					<nav>
+						<ul>
+							<li><a href="#">TOP</a></li>
+							<li><a href="#plan">デザインプラン</a></li>
+							<li><a href="#example01">導入事例</a></li>
+							<li><a href="#example02">施工事例</a></li>
+							<li><a href="#flow">施工までの流れ</a></li>
+							<li><a href="sekouhouhou.html">施工スタッフのこだわり</a></li>
+							<li><a href="#area">サービスエリア</a></li>
+							<li><a href="#company">会社概要</a></li>
+							<li><a href="contact.html">お問合せ</a></li>
+						</ul>
+					</nav>
+					<!-- SP用ドロップダウンメニュー -->
+					<nav class="nav-sp">
+						<ul>
+							<li><a href="#">TOP</a></li>
+							<li><a href="#plan">デザインプラン</a></li>
+							<li><a href="#example01">導入事例</a></li>
+							<li><a href="#example02">施工事例</a></li>
+							<li><a href="#flow">施工までの流れ</a></li>
+							<li><a href="sekouhouhou.html">施工スタッフのこだわり</a></li>
+							<li><a href="#area">サービスエリア</a></li>
+							<li><a href="#company">会社概要</a></li>
+							<li><a href="contact.html">お問合せ</a></li>
+						</ul>
+					</nav>
+					<!-- End SP用ドロップダウンメニュー -->
+				</div>
+			</div>
+	</header>
 
-	<header id="masthead" class="site-header" role="banner">
-
-		<?php get_template_part( 'template-parts/header/header', 'image' ); ?>
-
-		<?php if ( has_nav_menu( 'top' ) ) : ?>
-			<div class="navigation-top">
-				<div class="wrap">
-					<?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
-				</div><!-- .wrap -->
-			</div><!-- .navigation-top -->
-		<?php endif; ?>
-
-	</header><!-- #masthead -->
-
-	<?php
-
-	/*
-	 * If a regular post or page, and not the front page, show the featured image.
-	 * Using get_queried_object_id() here since the $post global may not be set before a call to the_post().
-	 */
-	if ( ( is_single() || ( is_page() && ! twentyseventeen_is_frontpage() ) ) && has_post_thumbnail( get_queried_object_id() ) ) :
-		echo '<div class="single-featured-image-header">';
-		echo get_the_post_thumbnail( get_queried_object_id(), 'twentyseventeen-featured-image' );
-		echo '</div><!-- .single-featured-image-header -->';
-	endif;
-	?>
-
-	<div class="site-content-contain">
-		<div id="content" class="site-content">
