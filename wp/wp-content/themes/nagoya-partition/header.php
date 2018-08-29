60d24afded2af5764406b10dbe6f6b0d6345b823
あきわ <akiwa.inc.jp@gmail.com>
2018-08-29 16:25:29 +0900

    tag manager 追加

--- wp/wp-content/themes/nagoya-partition/header.php
+++ wp/wp-content/themes/nagoya-partition/header.php
@@ -15,6 +15,20 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<!-- Google Tag Manager -->
+
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+
+})(window,document,'script','dataLayer','GTM-WRBF79');</script>
+
+<!-- End Google Tag Manager -->
+
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="target-densitydpi=device-dpi, width=640, maximum-scale=1.0, user-scalable=yes">
 <link rel="profile" href="http://gmpg.org/xfn/11">
